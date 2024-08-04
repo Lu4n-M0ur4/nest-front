5cba07ef5108ec8a7b3ed2c4c50fa48c2d122085
Lu4n-M0ur4 <luan.moura-rj@hotmail.com>
2024-08-03 21:14:02 -0300

    FirstCommit

---
+++ .eslintrc.cjs
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+    jest: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react-refresh',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'semi': ['error', 'always'],
+    'no-multiple-empty-lines': [
+      'error',
+      { max: 1 }
+    ],
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hello, Vite!</h1>
+        <img src="https://vitejs.dev/logo.svg" alt="Vite logo" width="100" />
+        <p>
+          Welcome to your Vite.js app!
+          <br />
+          <a href="https://vitejs.dev/guide/">Documentation</a>
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
